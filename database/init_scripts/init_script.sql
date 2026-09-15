292a3b86f760dda50d750cd29467a1497e0c76a4
Seng SeangLeng <seangleng507@gmail.com>
2026-09-15 10:02:07 +0700

    feat: add database folder

---
+++ database/init_scripts/init_script.sql
@@ -0,0 +1,4 @@
+create database db_order OWNER ecommerce_usr;
+create database db_payment OWNER ecommerce_usr;
+create database db_business OWNER ecommerce_usr;
+create database db_customer OWNER ecommerce_usr;
