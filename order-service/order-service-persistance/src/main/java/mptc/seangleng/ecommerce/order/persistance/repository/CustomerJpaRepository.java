952073bf674cc9fbb1ed96a175612076ee51f9ae
Seng SeangLeng <seangleng507@gmail.com>
2026-09-21 15:56:20 +0700

    fix: change filename convension

--- order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/repository/CustomerJpaRepository.java
+++ order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/repository/CustomerJpaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CustomerJPARepository extends JpaRepository<CustomerEntity, UUID> {
+public interface CustomerJpaRepository extends JpaRepository<CustomerEntity, UUID> {
 
 }
