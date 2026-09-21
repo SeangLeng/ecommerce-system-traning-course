3fb96c3a25d25cee22814abc56d6cdf78a7b464e
Seng SeangLeng <seangleng507@gmail.com>
2026-09-21 15:58:11 +0700

    fix: change import

--- order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/adapter/CustomerRepositoryAdapter.java
+++ order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/adapter/CustomerRepositoryAdapter.java
@@ -3,7 +3,7 @@ package mptc.seangleng.ecommerce.order.persistance.adapter;
 import lombok.RequiredArgsConstructor;
 import mptc.seangleng.ecommerce.order.entity.Customer;
 import mptc.seangleng.ecommerce.order.persistance.mapper.OrderPersistenceMapper;
-import mptc.seangleng.ecommerce.order.persistance.repository.CustomerJPARepository;
+import mptc.seangleng.ecommerce.order.persistance.repository.CustomerJpaRepository;
 import mptc.seangleng.ecommerce.order.port.output.CustomerRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Repository
 @RequiredArgsConstructor
 public class CustomerRepositoryAdapter implements CustomerRepository {
-    private final CustomerJPARepository customerJPARepository;
+    private final CustomerJpaRepository customerJPARepository;
     private final OrderPersistenceMapper orderPersistenceMapper;
 
     @Override
