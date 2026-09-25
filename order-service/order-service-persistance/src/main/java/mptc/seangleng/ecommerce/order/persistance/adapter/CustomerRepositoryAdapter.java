77d21aa109afaef9e55c3e39a0e3f40bc4a6886e
Seng SeangLeng <seangleng507@gmail.com>
2026-09-25 16:50:57 +0700

    feat: update

--- order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/adapter/CustomerRepositoryAdapter.java
+++ order-service/order-service-persistance/src/main/java/mptc/seangleng/ecommerce/order/persistance/adapter/CustomerRepositoryAdapter.java
@@ -2,6 +2,7 @@ package mptc.seangleng.ecommerce.order.persistance.adapter;
 
 import lombok.RequiredArgsConstructor;
 import mptc.seangleng.ecommerce.order.entity.Customer;
+import mptc.seangleng.ecommerce.order.persistance.mapper.CustomerPersistenceMapper;
 import mptc.seangleng.ecommerce.order.persistance.mapper.OrderPersistenceMapper;
 import mptc.seangleng.ecommerce.order.persistance.repository.CustomerJpaRepository;
 import mptc.seangleng.ecommerce.order.port.output.CustomerRepository;
@@ -13,11 +14,11 @@ import java.util.UUID;
 @Repository
 @RequiredArgsConstructor
 public class CustomerRepositoryAdapter implements CustomerRepository {
-    private final CustomerJPARepository customerJPARepository;
-    private final OrderPersistenceMapper orderPersistenceMapper;
+    private final CustomerPersistenceMapper customerPersistenceMapper;
+    private final CustomerJpaRepository customerJpaRepository;
 
     @Override
     public Optional<Customer> findById(UUID customerId) {
-        return customerJPARepository.findById(customerId).map(orderPersistenceMapper::customerEntityToCustomer);
+        return customerJpaRepository.findById(customerId).map(customerPersistenceMapper::customerEntityToCustomer);
     }
 }
